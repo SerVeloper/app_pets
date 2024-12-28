d2037ba07aee9a3c4aca9f0083b0294e60709c1a
sergio <sergio@sergioDev.sergio>
2024-12-28 17:07:32 -0400

    navBar corregido

--- lib/features/home/view/screens/HomeScreen.dart
+++ lib/features/home/view/screens/HomeScreen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'DashboardScreen.dart';
-import 'ListScreen.dart';
-import 'FavoritesScreen.dart';
-import 'ProfileScreen.dart';
-import '../widgets/BottomNavBar.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import '../../../../core/utils/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -14,18 +11,27 @@ class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
 
   final List<Widget> _screens = [
-    DashboardScreen(),
-    ListScreen(),
-    FavoritesScreen(),
-    ProfileScreen(),
+    Center(child: Text('Home')),
+    Center(child: Text('List')),
+    Center(child: Text('Favorites')),
+    Center(child: Text('Profile')),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavBar(
-        currentIndex: _currentIndex,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: backgroundColor, // Fondo detrás de la barra
+        color: secondaryColor, // Color de la barra
+        buttonBackgroundColor: primaryColor, // Color del botón activo
+        height: 50, // Altura de la barra
+        items: const <Widget>[
+          Icon(Icons.home, size: 30, color: backgroundColor, semanticLabel: "Home",),
+          Icon(Icons.list, size: 30, color: backgroundColor),
+          Icon(Icons.favorite, size: 30, color: backgroundColor),
+          Icon(Icons.person, size: 30, color: backgroundColor),
+        ],
         onTap: (index) {
           setState(() {
             _currentIndex = index;
